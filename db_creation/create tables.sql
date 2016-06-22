9b8babe33ded107773e8e8842e5d89296a4b09c5
Adam Mullarkey <acmullarkey@gmail.com>
2016-06-21 21:42:00 -0400

    Added creation statements for post-related tables

--- db_creation/create tables.sql
+++ db_creation/create tables.sql
@@ -24,12 +24,53 @@ FOREIGN KEY (user_id) REFERENCES users(id),
 FOREIGN KEY (project_id) REFERENCES projects(id)
 );
 
-create table messages
+create table posts
 (
 id int not null auto_increment,
-messages_text varchar(255) not null,
+created_by int,
+created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY (id),
+FOREIGN KEY (created_by) REFERENCES project_developers(id)
+);
+
+create table comments
+(
+	id int PRIMARY KEY REFERENCES posts (id),
+	parent_post_id int NOT NULL,
+	comment_text varchar(255) NOT NULL,
+	created_at TIMESTAMP REFERENCES posts (created_at),
+	created_by_developer int,
+	FOREIGN KEY (parent_post_id) REFERENCES posts (id) ON DELETE CASCADE,
+	FOREIGN KEY (created_by_developer) REFERENCES posts (created_by)
+);
+
+create table messages
+(
+id int PRIMARY KEY REFERENCES posts (id),
+messages_text varchar(255) NOT NULL,
 created_by_developer int,
-created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-primary key (id),
-foreign key (created_by_developer) references project_developers(id)
+created_at TIMESTAMP REFERENCES posts (created_at),
+FOREIGN KEY (created_by_developer) REFERENCES posts(created_by)
+);
+
+create table stories
+(
+id int PRIMARY KEY REFERENCES posts (id),
+story_name varchar(128) not null,
+story_description varchar(4096) not null,
+created_by_developer int,
+created_at TIMESTAMP REFERENCES posts (created_at),
+FOREIGN KEY (created_by_developer) REFERENCES posts(created_by)
+);
+
+create table tasks
+(
+id int PRIMARY KEY REFERENCES posts (id),
+story_id int,
+task_name varchar(128) not null,
+task_description varchar(4096) not null,
+created_by_developer int NOT NULL,
+created_at TIMESTAMP REFERENCES posts (created_at),
+FOREIGN KEY (created_by_developer) REFERENCES posts(created_by),
+FOREIGN KEY (story_id) REFERENCES stories(id)
 );
