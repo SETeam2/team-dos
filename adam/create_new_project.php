0291a67a5ce30637dc5c680ad469ee2cd6a30ed7
Adam Mullarkey <acmullarkey@gmail.com>
2016-07-17 23:15:18 -0400

    Created page to display all projects

--- adam/create_new_project.php
+++ adam/create_new_project.php
@@ -1,7 +1,9 @@
 <?php
 session_name('teamdos');
-session_start();
-
+session_start ();
+if (! isset ( $_SESSION['user']['name'] )) {
+    header ( "Location: login.html" ); // Redirect the user
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,44 +15,46 @@ session_start();
 <body>
 <?php
 // variables
-$project    = $_POST["project"];
-$password = $_POST["password"];
+if($_POST["project"] != '' && $_POST["password"]) {
+    $project = $_POST["project"];
+    $password = $_POST["password"];
 
 // Open connection to mysql
-$servername = "localhost";
-$db_username = "root";
-$db_password = "cs673";
-$db_name = "tracker_database";
+    $servername = "localhost";
+    $db_username = "root";
+    $db_password = "cs673";
+    $db_name = "tracker_database";
 
 // Create connection
-$conn = new mysqli($servername, $db_username, $db_password, $db_name);
+    $conn = new mysqli($servername, $db_username, $db_password, $db_name);
 
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
 // Check if email password combination are correct
-$query_info = "SELECT * FROM projects WHERE name = '$project' LIMIT 1";
+    $query_info = "SELECT * FROM projects WHERE name = '$project' LIMIT 1";
 
-$result = $conn->query($query_info);
+    $result = $conn->query($query_info);
 
-if ($result->num_rows > 0) {
-     // throw error
-     echo "Project by this name already exists.";
-} else {
-    $sql = "INSERT INTO projects (name, password)
+    if ($result->num_rows > 0) {
+        // throw error
+        echo "Project by this name already exists.";
+    } else {
+        $sql = "INSERT INTO projects (name, password)
                 VALUES ('$project', '$password')";
 
         if ($conn->query($sql) === TRUE) {
             if ($project != '') {
                 echo "New project " . $project . " created!";
-            } else {
-                echo "An error occurred; please enter your project credentials.";
             }
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
     }
+} else {
+    echo "An error occurred; please try again.";
 }
 // fetch associative array
 $row = $result->fetch_assoc();
